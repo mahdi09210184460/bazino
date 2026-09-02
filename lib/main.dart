1168fc11271b1e821b711f26260392ba484e4d06
mahdi <aminsoltani13920@gmail.com>
2026-09-03 01:04:57 +0330

    Update app display name to 'piko market' in Android Manifest and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'پیکو مارکت',
+      title: 'piko market',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
